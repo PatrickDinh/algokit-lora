3dd116d7599c5f927b9563606ea82e928bc2bd95
Hoang Dinh <d.conghoang@gmail.com>
2025-01-10 15:44:21 +1000

    chore: show version

--- src/features/settings/components/settings.tsx
+++ src/features/settings/components/settings.tsx
@@ -1,11 +1,19 @@
 import { ClearCache } from './clear-cache'
 import { NetworkConfigsTable } from '@/features/network/components/network-configs-table'
+import { useGetVersion } from '@/features/tauri/use-get-version'
 
 export function Settings() {
+  const version = useGetVersion()
+
   return (
     <div className="flex flex-col space-y-8">
       <NetworkConfigsTable />
       <ClearCache />
+      {version && (
+        <div className="flex flex-col items-end">
+          <p>Version: {version}</p>
+        </div>
+      )}
     </div>
   )
 }
